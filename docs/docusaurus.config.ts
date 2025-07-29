317e7c6a20cea0ba2462bc10e6bc2731b54346e3
mari0724 <ximenadelgadom07@gmail.com>
2025-07-29 08:56:39 -0500

    Ajustes para despliegue de documentación: gitignore y config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -14,19 +14,11 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://Mari0724.github.io',
   baseUrl: '/',
+  organizationName: 'Mari0724',
+  projectName: 'producttrack',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -94,7 +86,7 @@ const config: Config = {
         {
           title: 'Secciones',
           items: [
-            {label: 'Backend', to: '/docs/backend'},
+            { label: 'Backend', to: '/docs/backend' },
             { label: 'Frontend', to: '/docs/frontend' },
           ],
         },
@@ -113,10 +105,6 @@ const config: Config = {
             {
               label: 'Sobre ProductTrack',
               to: '/',
-            },
-            {
-              label: 'Guía de instalación',
-              href: '/docs/instalacion',
             },
           ],
         },
